517cd1122f2ac08f0a2c83535d4ea50e6628c3f6
Vaibhaw Agrawal <vaibhaw.agrawal@publicissapient.com>
2020-04-05 21:55:08 +0530

    Updates

---
+++ precache-manifest.9dc23e2667bc636b951b92328f922daf.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18cfc2f52ee7dcb2b1dbb5033d9602d6",
+    "url": "/kitchenaroma/index.html"
+  },
+  {
+    "revision": "bb92cec0d5b8a344156f",
+    "url": "/kitchenaroma/static/css/main.7cae2df1.chunk.css"
+  },
+  {
+    "revision": "6556a027cefdbc023039",
+    "url": "/kitchenaroma/static/js/2.f0e46cb7.chunk.js"
+  },
+  {
+    "revision": "bb92cec0d5b8a344156f",
+    "url": "/kitchenaroma/static/js/main.560fb807.chunk.js"
+  },
+  {
+    "revision": "d5089f4dd22d9a86666a",
+    "url": "/kitchenaroma/static/js/runtime~main.8cfaa5fd.js"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/kitchenaroma/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "a7e953bdbb49b4e18521029ee0bc1ffd",
+    "url": "/kitchenaroma/static/media/background.a7e953bd.jpg"
+  },
+  {
+    "revision": "01baef695cb9922f059148cf2147193b",
+    "url": "/kitchenaroma/static/media/cake1.01baef69.jpg"
+  },
+  {
+    "revision": "c535fba7be0f12dd64320e6157009e57",
+    "url": "/kitchenaroma/static/media/cake2.c535fba7.jpg"
+  },
+  {
+    "revision": "95b93cc94d512ea598a219e67f0554bd",
+    "url": "/kitchenaroma/static/media/cake3.95b93cc9.jpg"
+  },
+  {
+    "revision": "1d8d8b6f32ced66868d3da99ee5bd25c",
+    "url": "/kitchenaroma/static/media/cake4.1d8d8b6f.jpg"
+  },
+  {
+    "revision": "b76e309a9285e3b2d73cf34cade68df8",
+    "url": "/kitchenaroma/static/media/cake5.b76e309a.jpg"
+  },
+  {
+    "revision": "fa298092a9e3d446a4478a801921f629",
+    "url": "/kitchenaroma/static/media/cake6.fa298092.jpg"
+  },
+  {
+    "revision": "68bbcf480d8f273fd0bf533c67edaa18",
+    "url": "/kitchenaroma/static/media/cake7.68bbcf48.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/kitchenaroma/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/kitchenaroma/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/kitchenaroma/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "3a43e4a046c259a7fe53cc9487007e7c",
+    "url": "/kitchenaroma/static/media/cookies.3a43e4a0.jpg"
+  },
+  {
+    "revision": "df0cdbfb2c70d9857e3776eb83a5a4eb",
+    "url": "/kitchenaroma/static/media/creative-tim-white-slim2.df0cdbfb.png"
+  },
+  {
+    "revision": "b505953d063dcb69f7efd363ea498a0e",
+    "url": "/kitchenaroma/static/media/cupcake2.b505953d.jpg"
+  },
+  {
+    "revision": "d06cc4129d14bd3a71320a4b80be9a4d",
+    "url": "/kitchenaroma/static/media/cupcakes.d06cc412.jpg"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/kitchenaroma/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/kitchenaroma/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/kitchenaroma/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/kitchenaroma/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/kitchenaroma/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/kitchenaroma/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/kitchenaroma/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/kitchenaroma/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/kitchenaroma/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "ff5aafcf42e1f18e2f82d03c60813933",
+    "url": "/kitchenaroma/static/media/plumcake.ff5aafcf.jpg"
+  }
+]);
